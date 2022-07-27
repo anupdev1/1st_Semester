fcd6fcd451c57bf506e03b314f1740dc9d783c93
anupdev1 <anupdeb2463@gmail.com>
2022-07-27 18:47:10 +0600

    2a upload

---
+++ 2a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,p,n;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    if(a==c,b==d)
+    printf("p%d\n",p);
+    else
+        printf("n%d\n",n);
+    return 0;
+}
